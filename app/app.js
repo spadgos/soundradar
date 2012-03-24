f33b93f7d598d577bee7da24c55637cf9277bff0
Alexander Kovalev <kovalev@soundcloud.com>
2012-03-24 18:29:30 +0100

    featureObj format is changed

--- app/app.js
+++ app/app.js
@@ -6,8 +6,18 @@ $(function(){
       mapElement = document.getElementById("map_canvas"),
       center = new google.maps.LatLng(52.371, 4.895);
 
+  function getMarker(position) {
+      new google.maps.Marker({
+        map: map,
+        clickable: false,
+        position: position,
+        icon: 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_red.png'
+      })
+  }
+
   function getFeatures(category, type, bounds) {
-    var ne = bounds.getNorthEast(),
+    var boundsCenter = bounds.getCenter(),
+        ne = bounds.getNorthEast(),
         sw = bounds.getSouthWest(),
         n = ne.lat(),
         e = ne.lng(),
@@ -22,7 +32,13 @@ $(function(){
       dataType: 'xml'
     }).done(function (xml) {
       var features = $(xml).find('node').map(function (node) {
-        return new google.maps.LatLng(parseFloat(this.getAttribute('lat')), parseFloat(this.getAttribute('lon')));
+        var latLng = new google.maps.LatLng(parseFloat(this.getAttribute('lat')), parseFloat(this.getAttribute('lon')));
+        return {
+          type: category + '=' + type,
+          latLng: latLng,
+          heading: computeHeading(boundsCenter, latLng),
+          distance: computeDistanceBetween(boundsCenter, latLng)
+        };
       }).get();
       deferred.resolve(features);
     }).fail(deferred.reject);
@@ -42,14 +58,10 @@ $(function(){
 
   setTimeout(function(){
     getFeatures('amenity', 'bar', getMapBounds()).done(function(features){
-      console.log(features);
-      features.forEach(function(featureLatLng){
-        var featureMarker = new google.maps.Marker({
-          map: map,
-          clickable: false,
-          position: featureLatLng
-        })
-      })
+      features.forEach(function(featureObj){
+        var featureMarker = getMarker(featureObj.latLng);
+      });
+
     });
 
   }, 3000)
