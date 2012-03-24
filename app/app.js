803e7ef75284cdf5bd7bc92ac2155344603bd075
Alexander Kovalev <kovalev@soundcloud.com>
2012-03-24 18:09:52 +0100

    rendering of markers for features

--- app/app.js
+++ app/app.js
@@ -1,26 +1,57 @@
-function getFeatures(category, type, bounds) {
-  var ne = bounds.getNorthEast(),
-      sw = bounds.getSouthWest(),
-      n = ne.lat(),
-      e = ne.lng(),
-      s = sw.lat(),
-      w = sw.lng();
+$(function(){
 
-  var deferred = $.Deferred();
-  var url = CONFIG.api + '/node[' + category + '=' + type + '][bbox=' + [w, s, e, n].join(',') + ']';
+  var computeHeading = google.maps.geometry.spherical.computeHeading,
+      computeDistanceBetween = google.maps.geometry.spherical.computeDistanceBetween,
+      map,
+      mapElement = document.getElementById("map_canvas"),
+      center = new google.maps.LatLng(52.371, 4.895);
 
-  $.ajax({
-    url : url,
-    dataType: 'xml'
-  }).done(function (xml) {
-    var features = $(xml).find('node').map(function (node) {
-      return {
-        lat: parseFloat(this.getAttribute('lat')),
-        lon: parseFloat(this.getAttribute('lon'))
-      };
-    }).get();
-    deferred.resolve(features);
-  }).fail(deferred.reject);
+  function getFeatures(category, type, bounds) {
+    var ne = bounds.getNorthEast(),
+        sw = bounds.getSouthWest(),
+        n = ne.lat(),
+        e = ne.lng(),
+        s = sw.lat(),
+        w = sw.lng();
 
-  return deferred;
-}
+    var deferred = $.Deferred();
+    var url = CONFIG.api + '/node[' + category + '=' + type + '][bbox=' + [w, s, e, n].join(',') + ']';
+
+    $.ajax({
+      url : url,
+      dataType: 'xml'
+    }).done(function (xml) {
+      var features = $(xml).find('node').map(function (node) {
+        return new google.maps.LatLng(parseFloat(this.getAttribute('lat')), parseFloat(this.getAttribute('lon')));
+      }).get();
+      deferred.resolve(features);
+    }).fail(deferred.reject);
+
+    return deferred;
+  }
+
+  function getMapBounds() {
+    return map.getBounds();
+  }
+
+  map = new google.maps.Map(mapElement, {
+    center:    center,
+    zoom:      15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  setTimeout(function(){
+    getFeatures('amenity', 'bar', getMapBounds()).done(function(features){
+      console.log(features);
+      features.forEach(function(featureLatLng){
+        var featureMarker = new google.maps.Marker({
+          map: map,
+          clickable: false,
+          position: featureLatLng
+        })
+      })
+    });
+
+  }, 3000)
+
+});
